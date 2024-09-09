0be8289829d4bda595bff7301d4fbfab89864ed9
OuZhou <jbo20030322@icloud.com>
2024-09-09 21:16:36 +0800

    add some comments

--- instrumentshop/src/main/java/com/shop/service/impl/UserServiceImpl.java
+++ instrumentshop/src/main/java/com/shop/service/impl/UserServiceImpl.java
@@ -14,13 +14,17 @@ public class UserServiceImpl implements com.shop.service.UserService{
     @Override
     public void register(String username, String password) {
         //注册用户
+        //密码加密,得到MD5字符串
         String md5String = Md5Util.getMD5String(password);
+        //将用户信息存入数据库(用户名,密码md5字符串)
         userMapper.addUser(username, md5String);
     }
 
     @Override
     public User findUserByUsername(String username) {
+        //查找用户
         User user = userMapper.findUserByUsername(username);
+        //返回用户
         return user;
     }
 }
