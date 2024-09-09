0066e96683e69d0b861c8043270dd558e608b5d9
OuZhou <jbo20030322@icloud.com>
2024-09-09 22:51:33 +0800

    rename instrument in pojo

--- instrumentshop/src/main/java/com/shop/pojo/MusicalInstruments.java
+++ instrumentshop/src/main/java/com/shop/pojo/MusicalInstruments.java
@@ -4,7 +4,7 @@ import lombok.Data;
 
 import java.math.BigDecimal;
 @Data
-public class MusicalInstrument {
+public class MusicalInstruments {
     private int id;                  // 乐器ID
     private String name;             // 乐器名称
     private BigDecimal pricePerUnit; // 单价
